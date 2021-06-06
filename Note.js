dbb30b95a86d42de15419b25b9707c35a0f6b3de
MufeedBashir <84316713+MufeedBashir@users.noreply.github.com>
2021-06-06 17:14:36 +0530

    Add files via upload

---
+++ Note.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Note from "./Note";
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & ReactJS</h1>
+      <p>
+        This was an amazing Bootcamp taken up by Shaurya Sinha I would Love to
+        learn More from him ahead. Thank You Shaurya Sir and big thanks to
+        ShapeAI Team as well...
+      </p>
+    </div>
+  );
+}
+export default Note;
